43459deaa0a72219f3a165af6e5ed84fc2378591
Praveen Dilshan <rosapraveen1@gmail.com>
2024-09-02 10:04:05 +0530

    Add and Intro section for the portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/NavBar/navbar.js"
-
+import Intro from "./components/Intro/intro.js"
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import 
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/image.png'
+import {Link} from 'react-scroll';
+import btnImg from '../../assets/hireme.png'
 
-const intro = () => {
+const Intro = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="intro">
+        <div className= "introContent">
+           <span className="hello">Hello,</span>
+           <span className="introText">
+                I'm <span className="introName">Praveen</span> <br/>Website Designer
+                <p className="introPara">I am a skilled web designer with experience in creating <br/> visually appealing and user friendly websites.</p>
+                <Link><button className="btn"><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
+            </span> 
+        </div>
+        <img src={bg} alt="profile" className="bg" />
+    </section>
   )
 }
 
-export default intro
+export default Intro;
