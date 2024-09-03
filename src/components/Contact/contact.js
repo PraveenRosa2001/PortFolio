a4faacf03db3d93484684025b165e6404950690f
Praveen Dilshan <rosapraveen1@gmail.com>
2024-09-03 22:31:47 +0530

    Add an contact section for connecting my Gmail

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import Walmart from '../../assets/walmart.png'
 import Adobe from '../../assets/adobe.png'
@@ -8,9 +8,33 @@ import FacebookIcon from '../../assets/facebook-icon.png'
 import TwitterIcon from '../../assets/twitter.png'
 import YoutubeIcon from '../../assets/youtube.png'
 import InstagramIcon from '../../assets/instagram.png'
+import emailjs from '@emailjs/browser';
+
+
 
 const Contact = () => {
-  return (
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_5hdokac', 'template_i4j7fn5', form.current, {
+            publicKey: 'k3Y4sVLkESb1uC4jc',
+          })
+          .then(
+            (result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('Email Sent!');
+            },
+            (error) => {
+              console.log( error.text);
+            },
+          );
+      };
+    
+    return (
     <section id="contactPage">
         <div id="clients">
             <h1 className="contactPageTitle">My Clients</h1>
@@ -27,11 +51,11 @@ const Contact = () => {
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder='Your Name'/>
-                <input type="email" className="email" placeholder='Your Email'/>
+            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder='Your Name' name='your_name'/>
+                <input type="email" className="email" placeholder='Your Email'name='your_email'/>
                 <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
-                <button type="submit" value="Send" className="submitBtn">Submit</button>
+                <button type='submit' value='Send' className="submitBtn">Submit</button>
                 <div className="links">
                     <img src={FacebookIcon} alt="Facebook" className="link"/> 
                     <img src={TwitterIcon} alt="Twitter" className="link"/>
