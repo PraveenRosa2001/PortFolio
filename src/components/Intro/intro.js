a675784f9308f332b901e11bf5a52d70ab802e9a
Praveen Dilshan <rosapraveen1@gmail.com>
2024-09-05 13:33:41 +0530

    Modified the Intro section by adding effects and transition

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/image.png'
-import {Link} from 'react-scroll';
-import btnImg from '../../assets/hireme.png'
+import bg from '../../assets/image.png';
+import { Link } from 'react-scroll';
+import btnImg from '../../assets/hireme.png';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Intro = () => {
   return (
     <section id="intro">
-        <div className= "introContent">
-           <span className="hello">Hello,</span>
-           <span className="introText">
-                I'm <span className="introName">Praveen</span> <br/>Website Designer
-                <p className="introPara">I am a skilled web designer with experience in creating <br/> visually appealing and user friendly websites.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
-            </span> 
-        </div>
-        <img src={bg} alt="profile" className="bg" />
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Praveen</span> <br />
+          <span className="typewriter">
+            <Typewriter
+              words={['Website Designer']}
+              loop={Infinity}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+          <p className="introPara">
+            I am a skilled web designer with experience in creating <br />
+            visually appealing and user-friendly websites.
+          </p>
+        </span>
+      </div>
+      <img src={bg} alt="profile" className="bg" />
     </section>
-  )
-}
+  );
+};
 
 export default Intro;
